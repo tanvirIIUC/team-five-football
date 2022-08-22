2874be983c9a36c935f00cbf73fb1e69c8b7b100
tanvirIIUC <tanvirsohaniiuc@gmail.com>
2022-08-22 17:11:15 +0600

    total player cost

--- js/index.js
+++ js/index.js
@@ -21,14 +21,13 @@
     li.innerText=playerName;
      
     const n= document.querySelectorAll('.list-decimal li')
-    console.log(n);
-    console.log(n.length);
+    
         if(n.length<5)
         {
          list.appendChild(li);
         }
         else {
-         alert("Full");
+         alert(" List Full !!!");
         }    
  }
 
@@ -38,52 +37,90 @@
     const  player = document.getElementById('Kylian Mbappé');
     const playerName= player.innerText;
     addFriendList(playerName);   
+    document.getElementById("mbappy").disabled = true;
+    document.getElementById("mbappy").style.background = "white";
+    
  })
  document.getElementById('Messi').addEventListener('click',function()
  {
     const  player = document.getElementById('Lionel-Messi');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Messi").disabled = true;
+    document.getElementById("Messi").style.background = "white";
+    
  })
  document.getElementById('Neymar').addEventListener('click',function()
  {
     const  player = document.getElementById('Neymar-Jr');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Neymar").disabled = true;
+    document.getElementById("Neymar").style.background = "white";
+    
  })
  document.getElementById('Sergio').addEventListener('click',function()
  {
     const  player = document.getElementById('Sergio-Ramos');
     const playerName= player.innerText;
-    addFriendList(playerName);  
+    addFriendList(playerName); 
+    document.getElementById("Sergio").disabled = true;
+    document.getElementById("Sergio").style.background = "white";
+     
  })
  document.getElementById('Vítor').addEventListener('click',function()
  {
     const  player = document.getElementById('Vítor-Machado');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Vítor").disabled = true;
+    document.getElementById("Vítor").style.background = "white";
+    
  })
  document.getElementById('Renato').addEventListener('click',function()
  {
     const  player = document.getElementById('Renato-Sanches');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Renato").disabled = true;
+    document.getElementById("Renato").style.background = "white";
+    
  })
  document.getElementById('Thibaut').addEventListener('click',function()
  {
     const  player = document.getElementById('Thibaut-Courtois');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Thibaut").disabled = true;
+    document.getElementById("Thibaut").style.background = "white";
+    
  })
  document.getElementById('Karim').addEventListener('click',function()
  {
     const  player = document.getElementById('Karim-Benzema');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Karim").disabled = true;
+    document.getElementById("Karim").style.background = "white";
+    
  })
  document.getElementById('Kaka1').addEventListener('click',function()
  {
     const  player = document.getElementById('Kaka');
     const playerName= player.innerText;
     addFriendList(playerName);  
+    document.getElementById("Kaka1").disabled = true;
+    document.getElementById("Kaka1").style.background = "white";
+    
  })
+
+
+ document.getElementById('total-player-cost').addEventListener('click',function()
+ {
+   const perPlayer= document.getElementById('Per-Player');
+   const perPlayerCost =parseInt(perPlayer.value) ;
+   //  console.log(perPlayerCost);
+    const playerCostTotal = document.getElementById('player-cost-total');
+    playerCostTotal.innerText= perPlayerCost*5;
+ }
+ )
